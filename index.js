80afd19e1c7f727e0d3d3c1cdf07ab3859f7c417
amreenkhan145899-tech <amreenkhan145899@gmail.com>
2026-09-21 16:31:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("MindEase Backend is Running ✅");
+});
 
 const ai = new GoogleGenAI({
     apiKey: process.env.GEMINI_API_KEY
